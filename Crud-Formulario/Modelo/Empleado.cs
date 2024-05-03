af86e8e8f61fe2c34d969a404dbb0dde2825de2b
69anthony <anthonyvalverde36@gmail.com>
2024-05-02 20:00:45 -0500

    6p

--- Crud-Formulario/Modelo/Empleado.cs
+++ Crud-Formulario/Modelo/Empleado.cs
@@ -25,7 +25,20 @@ namespace Modelo
             string consulta = string.Format("SELECT id_puesto as id, puesto FROM puesto;");
             //Para ejecutar la consulta
             MySqlDataAdapter query = new MySqlDataAdapter(consulta, conectar.conectar);
+            //Para ejecutar el Query
+            query.Fill(dt);
+            conectar.CerarConexion();
+            return dt;
+        }
 
+        private DataTable grid_empleados() 
+        {
+            DataTable dt = new DataTable();
+            conectar = new conexionBD();
+            conectar.AbrirConexion();
+            string consulta = string.Format("SELECT e.id_empleado AS id, e.codigo, e.nombres, e.apellidos, e.direccion, e.telefonos," +
+                "e.fecha_nacimiento, p.puesto, e.id_puesto FROM empleados as e INNER JOIN puesto as p on e.id_puesto = p.id_puesto;");
+            MySqlDataAdapter query = new MySqlDataAdapter(consulta, conectar.conectar);
             query.Fill(dt);
             conectar.CerarConexion();
             return dt;
